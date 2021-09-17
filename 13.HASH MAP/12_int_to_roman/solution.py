019ad6333d0b74ac44675314bc3f6147a2c1f5d1
unknown <kimmyoo@gmail.com>
2021-09-16 23:52:25 -0400

    12 int to roman

---
+++ 13.HASH MAP/12_int_to_roman/solution.py
@@ -0,0 +1,39 @@
+from collections import OrderedDict
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+
+        # if you need an ordered dictionary, 
+        # you need to pass a list of tuple to OrderedDict constructor
+        d = [
+            (1000, 'M'), 
+            (900, 'CM'), 
+            (500, 'D'), 
+            (400, 'CD'), 
+            (100, 'C'), 
+            (90,'XC'), 
+            (50, 'L'), 
+            (40, 'XL'), 
+            (10, 'X'), 
+            (9, 'IX'), 
+            (5, 'V'), 
+            (4, 'IV'), 
+            (1, 'I')]
+        
+        translation = OrderedDict(d)
+        
+        res = ""
+        
+        for i in translation:
+            res += (num//i) * translation[i]
+            num %= i
+
+        return res
+
+s = Solution()
+res = s.intToRoman(87)
+print(res)
